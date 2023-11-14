19a11f2a6bb807855b6d58caa28df565e9b6254b
Nikita <lobanovns01@gmail.com>
2023-11-15 00:46:42 +0300

    ComboReady

--- script.js
+++ script.js
@@ -17,31 +17,30 @@ let comboCheck = function(card1, card2, card3) {
         }else if (cards[i].value =="ACE" ){
             cards[i].value = "14"
         }
-
         cards[i].value=Number(cards[i].value);
     }
-
-    cards.sort(function(a, b){return a - b});
-
-    //стрит флеш
-    if (((Number(card1.value) == Number(card2.value)+1 == Number(card3.value)+2) || ((Number(card1.value) == 2) && (Number(card2.value) == 3) && (Number(card3.value) == 14))) 
-    && (card1.suit == card1.suit == card1.suit))
-    {
+    
+    cards.sort(function(a, b){return Number(a.value) - Number(b.value)});
+    card1 = cards[0];// по хорошему это все переписать но пока отлаживаю оставлю
+    card2 = cards[1];
+    card3 = cards[2];
+    // стрит флеш
+    if (((Number(card1.value) == Number(card2.value)-1 ) && (Number(card2.value)== Number(card3.value)-1)) ||
+    ((Number(card1.value) == 2) && (Number(card2.value) == 3) && (Number(card3.value) == 14))
+    && ((card1.suit == card2.suit) && (card2.suit == card3.suit))){
         return [5,"Стрит флеш"];
     }
-    //тройка
-    if (Number(card1.value) == Number(card2.value) == Number(card3.value))
-    {
+    // тройка 
+    if ((Number(card1.value) == Number(card2.value)) && (Number(card2.value) == Number(card3.value))){
         return [4,"Тройка"];
     }
-    // стрит не работает
-    if ((Number(card1.value) == Number(card2.value)+1 == Number(card3.value)+2) || ((Number(card1.value) == 2) && (Number(card2.value) == 3) && (Number(card3.value) == 14)))
-    {
+    // стрит
+    if (((Number(card1.value) == (Number(card2.value) + 1)) && (Number(card2.value) == (Number(card3.value) + 1 ))) ||
+    ((Number(card1.value) == 2) && (Number(card2.value) == 3) && (Number(card3.value) == 14))){
         return [3,"Стрит"];
     }
     // флеш
-    if (card1.suit == card1.suit == card1.suit)
-    {
+    if ((card1.suit == card2.suit) && (card2.suit == card3.suit)){
         return [2,"Флеш"];
     }
     // пара
@@ -51,7 +50,6 @@ let comboCheck = function(card1, card2, card3) {
     // старшая карта
     return [0,"Старшая карты"];
   };
-
 
 fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
     .then(response => response.json())
@@ -64,7 +62,6 @@ fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
         .then(threeMyDeck => {
 
 
-             
             document.getElementById("card0").src=`${threeMyDeck.cards[0].image}`;
             document.getElementById("card1").src=`${threeMyDeck.cards[1].image}`;
             document.getElementById("card2").src=`${threeMyDeck.cards[2].image}`;
@@ -73,7 +70,6 @@ fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
             const myComboBlockText = document.querySelector(".cards_info");
             let myCombo = comboCheck(threeMyDeck.cards[0],threeMyDeck.cards[1],threeMyDeck.cards[2]);
             myComboBlockText.innerText = myCombo[1];
-
 
     const form = document.querySelector(".form_play");
 
@@ -93,7 +89,6 @@ fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
             const dealerComboBlockText = document.querySelector(".dealer_cards_info");
             let dealerCombo = comboCheck(treeDealerCards.cards[0],treeDealerCards.cards[1],treeDealerCards.cards[2]);
             dealerComboBlockText.innerText = dealerCombo[1];
-
             
             let massage = parseInt(parent.querySelector(".money").value);
             if (myCombo[0] > dealerCombo[0])
@@ -104,7 +99,6 @@ fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
                 money = money - massage;
                 alert("Проигрыш");
             } else {
-                //тут нужно отсартировать карты
 
                 myHightCard = Math.max(Number(threeMyDeck.cards[0].value),Number(threeMyDeck.cards[1].value),Number(threeMyDeck.cards[2].value));
                 dealerHightCard = Math.max(Number(treeDealerCards.cards[0].value),Number(treeDealerCards.cards[1].value),Number(treeDealerCards.cards[2].value));
@@ -123,19 +117,12 @@ fetch(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`)
             }
             const moneyBlock = document.querySelector(".count");
             moneyBlock.innerText = money;
-
-
         }).catch(error => console.error(error));
-
-
-
-
     }
 
     form.addEventListener("submit", handleSubmit);
 
     }).catch(error => console.error(error));
-
 }).catch(error => console.error(error));
 
 
